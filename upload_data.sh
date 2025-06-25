cf97e803ed434b0fe48b4fa881a6ac51952b6d5f
venno-sudo <venman45@gmail.com>
2025-06-26 00:33:44 +0800

    Add files via upload

--- upload_data.sh
+++ upload_data.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 sleep 10
 if grep -qs '/media/USER/Weinmann ' /proc/mounts; then
-    cd /home/USER/prisma/scripts
-    /home/USER/prisma/mypython/bin/python3 transfer_data.py
-    /home/USER/prisma/mypython/bin/python3 prisma20a_sleephq_uploader.py
+    file_today="/media/USER/Weinmann/therapy.pdat"
+    file_prev="/home/USER/prisma/data/therapy.pdat"
+	if [[ ! -f "$file_prev" || "$file_today" -nt "$file_prev" ]]; then
+		cd /home/USER/prisma/scripts
+		/home/USER/prisma/mypython/bin/python3 /home/USER/prisma/scripts/transfer_data.py
+		/home/USER/prisma/mypython/bin/python3 /home/USER/prisma/scripts/prisma20a_sleephq_uploader.py
+		./nas_sync.sh
+	fi
+	sleep 1
     umount /dev/sda
+	sleep 1
     sudo ykushcmd ykushxs -d
-    ./nas_sync.sh
 else
     echo "It's not mounted."
 fi
